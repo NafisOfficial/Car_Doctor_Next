51698b3981f598dd46c5e5f352af65eb465ca1e3
Md Nafis Iqbal <nafisiqbal.net2002@gmail.com>
2024-09-12 13:07:21 +0600

    create services

--- src/Components/Cards/ServicesCards.jsx
+++ src/Components/Cards/ServicesCards.jsx
@@ -1,15 +1,21 @@
 import Image from 'next/image';
+import { FaArrowRight } from "react-icons/fa";
 
-const ServicesCards = () => {
+const ServicesCards = ({service}) => {
+    const {title, img, price} = service || {};
+
+    console.log(title,img,price)
+
+
     return (
         <div>
             <div className='flex flex-col gap-2 rounded w-56 border-2 p-2'>
-                <Image alt="services" className='rounded' src={s1} height={150} width={200} />
+                <Image alt="services" className='rounded' src={img} height={200} width={200} />
                 <div className='text-left'>
-                    <h1 className='font-bold text-lg text-black'>Services title</h1>
-                    <div className='flex justify-between'>
-                        <span className='text-primary'>Price: $20.00</span>
-                        <span>arrow</span>
+                    <h1 className='font-bold text-lg text-black'>{title}</h1>
+                    <div className='flex justify-between items-center'>
+                        <span className='text-primary'>Price: $ {price}</span>
+                        <span className='text-primary cursor-pointer' title='view details' ><FaArrowRight /></span>
                     </div>
                 </div>
             </div>
--- src/Components/HomePage/Services.jsx
+++ src/Components/HomePage/Services.jsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import s1 from '../../../public/assets/images/services/1.jpg'
-import Image from 'next/image';
+import {servicesData} from '../../lib/services.js'
+import ServicesCards from '../Cards/ServicesCards';
 
 const Services = () => {
     return (
-        <div className='flex flex-col text-center gap-8'>
+        <div className='flex flex-col text-center gap-8 mb-10'>
             <div className='flex flex-col gap-3 w-1/2 mx-auto'>
                 <h3 className='font-bold text-primary'>Service</h3>
                 <h1 className='font-bold text-5xl text-black'>Our Service Area</h1>
                 <p>the majority have suffered alteration in some form, by injected humour, or randomised words which don&apos;t look even slightly believable. </p>
             </div>
-            <div>
-                <div className='flex flex-col gap-2 rounded w-56 border-2 p-2'>
-                    <Image alt="services" className='rounded' src={s1} height={150} width={200} />
-                    <div className='text-left'>
-                        <h1 className='font-bold text-lg text-black'>Services title</h1>
-                        <div className='flex justify-between'>
-                            <span className='text-primary'>Price: $20.00</span>
-                            <span>arrow</span>
-                        </div>
-                    </div>
-                </div>
+            <div className='grid grid-cols-3 items-center w-2/3 mx-auto gap-5'>
+                {servicesData.map((service)=><ServicesCards key={service._id} service={service}/>)}
             </div>
         </div>
     );
